3e26a3b283c3c941dc82cf74ca9ed1c89ae3056d
tommyshhhhhhhhhhhhhhih <118866559+tommyshhhhhhhhhhhhhhih@users.noreply.github.com>
2022-11-23 10:12:10 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+// [START] 跳窗的設定 ==========
+
+// 關閉按鈕foucs
+$.fancybox.defaults.autoFocus = false;
+$.fancybox.defaults.backFocus = false;
+$.fancybox.defaults.baseClass = 'anytix';
+
+
+// 跳窗動畫
+$.fancybox.defaults.animationEffect = "fade";
+$.fancybox.defaults.animationDuration = 366;
+
+// [END] 跳窗的設定 ==========
